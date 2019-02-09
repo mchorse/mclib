c707b26b5eae751d508c507df9edc6d9c4ec8e1d
McHorse <mchorselessrider@gmail.com>
2019-02-09 18:53:21 +0000

    Introducing core mod

---
+++ src/main/java/mchorse/mclib/core/McLibCMClassTransformer.java
@@ -0,0 +1,22 @@
+package mchorse.mclib.core;
+
+import mchorse.mclib.core.transformers.SimpleReloadableResourceManagerTransformer;
+import mchorse.mclib.utils.coremod.CoreClassTransformer;
+
+public class McLibCMClassTransformer extends CoreClassTransformer
+{
+    private SimpleReloadableResourceManagerTransformer resourcePack = new SimpleReloadableResourceManagerTransformer();
+
+    @Override
+    public byte[] transform(String name, String transformedName, byte[] basicClass)
+    {
+        if (this.checkName(name, "bxi", "net.minecraft.client.resources.SimpleReloadableResourceManager"))
+        {
+            System.out.println("McLib: Transforming SimpleReloadableResourceManager class (" + name + ")");
+
+            return this.resourcePack.transform(name, basicClass);
+        }
+
+        return basicClass;
+    }
+}
