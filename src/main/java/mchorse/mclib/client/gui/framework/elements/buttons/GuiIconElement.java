59d9210af1060688f005d11136a370731190f23f
McHorse <mchorselessrider@gmail.com>
2020-03-30 13:22:07 +0100

    Add more setters to icon element

--- src/main/java/mchorse/mclib/client/gui/framework/elements/buttons/GuiIconElement.java
+++ src/main/java/mchorse/mclib/client/gui/framework/elements/buttons/GuiIconElement.java
@@ -1,6 +1,7 @@
 package mchorse.mclib.client.gui.framework.elements.buttons;
 
 import mchorse.mclib.client.gui.framework.elements.utils.GuiContext;
+import mchorse.mclib.client.gui.framework.elements.utils.GuiDraw;
 import mchorse.mclib.client.gui.utils.Icon;
 import mchorse.mclib.utils.ColorUtils;
 import net.minecraft.client.Minecraft;
@@ -12,7 +13,7 @@ public class GuiIconElement extends GuiClickElement<GuiIconElement>
 {
 	public Icon icon;
 	public Icon iconHover;
-	public int hoverColor = 0xffe6e6e6;
+	public int hoverColor = 0xffaaaaaa;
 
 	public GuiIconElement(Minecraft mc, Icon icon, Consumer<GuiIconElement> callback)
 	{
@@ -22,9 +23,30 @@ public class GuiIconElement extends GuiClickElement<GuiIconElement>
 		this.iconHover = icon;
 	}
 
-	public GuiIconElement setHovered(Icon icon)
+	public GuiIconElement both(Icon icon)
+	{
+		this.icon = this.iconHover = icon;
+
+		return this;
+	}
+
+	public GuiIconElement icon(Icon icon)
+	{
+		this.icon = icon;
+
+		return this;
+	}
+
+	public GuiIconElement hovered(Icon icon)
 	{
 		this.iconHover = icon;
+
+		return this;
+	}
+
+	public GuiIconElement hoverColor(int color)
+	{
+		this.hoverColor = color;
 
 		return this;
 	}
@@ -33,16 +55,19 @@ public class GuiIconElement extends GuiClickElement<GuiIconElement>
 	protected void drawSkin(GuiContext context)
 	{
 		Icon icon = this.hover ? this.iconHover : this.icon;
+		int color = this.hover ? this.hoverColor : 0xffffffff;
 
 		if (this.isEnabled())
 		{
-			ColorUtils.bindColor(this.hover ? 0xffaaaaaa : 0xffffffff);
+			ColorUtils.bindColor(color);
+			icon.render(this.area.mx(), this.area.my(), 0.5F, 0.5F);
 		}
 		else
 		{
-			GlStateManager.color(0, 0, 0, 1F);
-		}
+			GuiDraw.drawOutlinedIcon(icon, color, this.area.mx(), this.area.my(), 0.5F, 0.5F);
 
-		icon.render(this.area.mx(), this.area.my(), 0.5F, 0.5F);
+			GlStateManager.color(0, 0, 0, 0.5F);
+			icon.render(this.area.mx(), this.area.my(), 0.5F, 0.5F);
+		}
 	}
 }
