3b995b0d8000e3f9947815418724033122312ceb
McHorse <mchorselessrider@gmail.com>
2020-04-04 18:58:02 +0100

    Add label convenience methods

--- src/main/java/mchorse/mclib/client/gui/utils/Label.java
+++ src/main/java/mchorse/mclib/client/gui/utils/Label.java
@@ -1,5 +1,7 @@
 package mchorse.mclib.client.gui.utils;
 
+import java.util.Objects;
+
 public class Label<T>
 {
 	public String title;
@@ -9,5 +11,18 @@ public class Label<T>
 	{
 		this.title = title;
 		this.value = value;
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (obj instanceof Label)
+		{
+			Label label = (Label) obj;
+
+			return Objects.equals(this.title, label.title) && Objects.equals(this.value, label.value);
+		}
+
+		return super.equals(obj);
 	}
 }
