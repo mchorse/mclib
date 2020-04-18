a52f483fbbc9e80fdf6870b10ea6bdbba91cad5a
McHorse <mchorselessrider@gmail.com>
2020-04-18 07:37:16 +0100

    Move GUI interpolation list from Blockbuster

---
+++ src/main/java/mchorse/mclib/client/gui/framework/elements/list/GuiInterpolationList.java
@@ -0,0 +1,45 @@
+package mchorse.mclib.client.gui.framework.elements.list;
+
+import mchorse.mclib.utils.Interpolation;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.I18n;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * Interpolations list
+ */
+public class GuiInterpolationList extends GuiListElement<Interpolation>
+{
+    public GuiInterpolationList(Minecraft mc, Consumer<List<Interpolation>> callback)
+    {
+        super(mc, callback);
+
+        this.scroll.scrollItemSize = 16;
+
+        for (Interpolation interp : Interpolation.values())
+        {
+            this.add(interp);
+        }
+
+        this.sort();
+        this.background();
+    }
+
+    @Override
+    protected boolean sortElements()
+    {
+        Collections.sort(this.list, Comparator.comparing(o -> o.key));
+
+        return true;
+    }
+
+    @Override
+    protected String elementToString(Interpolation element, int i, int x, int y, boolean hover, boolean selected)
+    {
+        return I18n.format("blockbuster.gui.interpolations." + element.key);
+    }
+}
