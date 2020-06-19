83e7cf81781af657bc5d75f52db979e397db417b
McHorse <mchorselessrider@gmail.com>
2020-06-19 08:04:24 +0100

    Add MathUtils.cycler()

--- src/main/java/mchorse/mclib/utils/MathUtils.java
+++ src/main/java/mchorse/mclib/utils/MathUtils.java
@@ -16,4 +16,19 @@ public class MathUtils
 	{
 		return x < min ? min : (x > max ? max : x);
 	}
+
+	public static int cycler(int x, int min, int max)
+	{
+		return x < min ? max : (x > max ? min : x);
+	}
+
+	public static float cycler(float x, float min, float max)
+	{
+		return x < min ? max : (x > max ? min : x);
+	}
+
+	public static double cycler(double x, double min, double max)
+	{
+		return x < min ? max : (x > max ? min : x);
+	}
 }
