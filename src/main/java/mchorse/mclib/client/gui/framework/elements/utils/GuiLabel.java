ca56e58fca1a012fc9ad389e02b54659a1dd1999
McHorse <mchorselessrider@gmail.com>
2020-06-24 15:00:42 +0100

    Add dynamic color for label background

--- src/main/java/mchorse/mclib/client/gui/framework/elements/utils/GuiLabel.java
+++ src/main/java/mchorse/mclib/client/gui/framework/elements/utils/GuiLabel.java
@@ -3,7 +3,8 @@ package mchorse.mclib.client.gui.framework.elements.utils;
 import mchorse.mclib.client.gui.framework.elements.GuiElement;
 import mchorse.mclib.client.gui.utils.keys.IKey;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Gui;
+
+import java.util.function.Supplier;
 
 public class GuiLabel extends GuiElement
 {
@@ -12,6 +13,7 @@ public class GuiLabel extends GuiElement
 	public float anchorX;
 	public float anchorY;
 	public int background;
+	public Supplier<Integer> backgroundColor;
 
 	public GuiLabel(Minecraft mc, IKey label)
 	{
@@ -40,12 +42,24 @@ public class GuiLabel extends GuiElement
 		return this;
 	}
 
+	public GuiLabel background(Supplier<Integer> color)
+	{
+		this.backgroundColor = color;
+
+		return this;
+	}
+
 	public GuiLabel anchor(float x, float y)
 	{
 		this.anchorX = x;
 		this.anchorY = y;
 
 		return this;
+	}
+
+	public int getColor()
+	{
+		return this.backgroundColor == null ? this.background : this.backgroundColor.get();
 	}
 
 	@Override
@@ -65,7 +79,7 @@ public class GuiLabel extends GuiElement
 		int x = this.area.x(this.anchorX, this.font.getStringWidth(label));
 		int y = this.area.y(this.anchorY, this.font.FONT_HEIGHT);
 
-		GuiDraw.drawTextBackground(this.font, label, x, y, this.color, this.background);
+		GuiDraw.drawTextBackground(this.font, label, x, y, this.color, this.getColor());
 
 		super.draw(context);
 	}
