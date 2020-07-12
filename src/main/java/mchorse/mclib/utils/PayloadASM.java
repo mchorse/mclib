22c2f2122d59f75ae420df1747ec013fb6cc59e0
McHorse <mchorselessrider@gmail.com>
2020-07-12 18:46:27 +0100

    Add comment to PayloadASM

--- src/main/java/mchorse/mclib/utils/PayloadASM.java
+++ src/main/java/mchorse/mclib/utils/PayloadASM.java
@@ -6,6 +6,9 @@ public class PayloadASM
 {
 	public static final int MIN_SIZE = 32767;
 
+	/**
+	 * ASM hook which is used in {@link mchorse.mclib.core.transformers.CPacketCustomPayloadTransformer}
+	 */
 	public static int getPayloadSize()
 	{
 		return Math.max(MIN_SIZE, McLib.maxPacketSize.get());
