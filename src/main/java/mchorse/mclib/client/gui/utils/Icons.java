f9c27dd65d9f109fea200fd1ee27499336ba83a8
McHorse <mchorselessrider@gmail.com>
2020-12-17 19:16:25 +0000

    Move play, pause and disabled icons from Aperture

--- src/main/java/mchorse/mclib/client/gui/utils/Icons.java
+++ src/main/java/mchorse/mclib/client/gui/utils/Icons.java
@@ -52,7 +52,10 @@ public class Icons
 	public static final Icon FAVORITE = new Icon(ICONS, 0, 32);
 	public static final Icon VISIBLE = new Icon(ICONS, 16, 32);
 	public static final Icon INVISIBLE = new Icon(ICONS, 32, 32);
+	public static final Icon PLAY = new Icon(ICONS, 48, 32);
+	public static final Icon PAUSE = new Icon(ICONS, 64, 32);
 
 	public static final Icon CHECKBOARD = new Icon(ICONS, 0, 240, 16, 16);
-	public static final Icon CURSOR = new Icon(ICONS, 16, 240, 16, 16);
+	public static final Icon DISABLED = new Icon(ICONS, 16, 240, 16, 16);
+	public static final Icon CURSOR = new Icon(ICONS, 32, 240, 16, 16);
 }
