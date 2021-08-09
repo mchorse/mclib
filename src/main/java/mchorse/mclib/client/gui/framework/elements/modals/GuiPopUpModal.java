4ac671764991d68ead56c2e0867cb1378f088ea8
Chryfi <smile.plays.yt@gmail.com>
2021-08-09 21:49:21 +0200

    Refactor GuiPopUpModal

--- src/main/java/mchorse/mclib/client/gui/framework/elements/modals/GuiPopUpModal.java
+++ src/main/java/mchorse/mclib/client/gui/framework/elements/modals/GuiPopUpModal.java
@@ -15,31 +15,25 @@ import org.lwjgl.opengl.GL11;
 
 public class GuiPopUpModal extends GuiModal
 {
+    private static final Color BACKGROUND_COLOR = new Color(0F, 0F, 0F, 1F);
+    private static final Color TEXT_COLOR = new Color(1F, 1F, 1F, 1F);
+
     private boolean init = false;
     private int x0;
     private int y0;
     private Timer timer;
 
-    private final int BACKGROUND_COLOR = ColorUtils.COLOR.set(0F, 0F, 0F, this.a).getRGBAColor();
-    private final int TEXT_COLOR = 0xffffff;
-
-    /** Alpha */
-    private float a = 1F;
-
-    /** Drop-shadow color */
-    private int ds_c = McLib.primaryColor.get();
-
-    /** Background color */
-    private int bg_c = BACKGROUND_COLOR;
-
-    /** Text color */
-    private int tx_c = TEXT_COLOR;
+    /* Colours */
+    private float alpha = 1F;
+    private float shadowAlpha = 0.26666668F;
+    private Color shadowColor = (new Color(McLib.primaryColor.get())).set(this.shadowAlpha, 4);
+    private Color backgroundColor = BACKGROUND_COLOR.copy();
+    private Color textColor = TEXT_COLOR.copy();
 
     /**
      * fade duration in milliseconds
      */
     private int duration = 500;
-
 
     public GuiPopUpModal(Minecraft mc, IKey label)
     {
@@ -64,8 +58,8 @@ public class GuiPopUpModal extends GuiModal
     {
         if (!this.init)
         {
-            this.x0 = context.mouseX()-this.area.w/4;
-            this.y0 = context.mouseY()-this.area.h/2;
+            this.x0 = context.mouseX() - this.area.w / 4;
+            this.y0 = context.mouseY() - this.area.h / 2;
 
             this.init = true;
         }
@@ -77,30 +71,31 @@ public class GuiPopUpModal extends GuiModal
         GlStateManager.enableBlend();
         GlStateManager.enableAlpha();
 
-        Gui.drawRect(this.area.x, this.area.y, this.area.ex(), this.area.ey(), bg_c);
-        GuiDraw.drawDropShadow(area.x, area.y, area.ex(), area.ey(), 6, 0x44000000 + ds_c, ds_c);
-
+        GuiDraw.drawDropShadow(this.area.x, this.area.y, this.area.ex(), this.area.ey(), 6, this.shadowColor.getRGBAColor(), this.shadowColor.getRGBAColor());
+        Gui.drawRect(this.area.x, this.area.y, this.area.ex(), this.area.ey(), this.backgroundColor.getRGBAColor());
+        System.out.println(shadowColor.a);
         this.y = 0;
         int y = this.area.y + 10; //font offset
 
         for (String line : this.font.listFormattedStringToWidth(this.label.get(), this.area.w - 20))
         {
-            this.font.drawStringWithShadow(line, this.area.x + 10, y + this.y, tx_c);
+            this.font.drawStringWithShadow(line, this.area.x + 10, y + this.y, this.textColor.getRGBAColor());
             this.y += 11;
         }
 
         if (!this.area.isInside(context))
         {
-            if (timer == null)
+            if (this.timer == null)
             {
-                timer = new Timer(duration);
-                timer.mark();
+                this.timer = new Timer(this.duration);
+                this.timer.mark();
             }
 
-            float x = ((float)duration - timer.getRemaining())/duration;
+            float x = ((float)this.duration - this.timer.getRemaining()) / this.duration;
 
-            this.bg_c = fadeAlpha(this.bg_c, x);
-            this.tx_c = fadeAlpha(this.tx_c, x);
+            fadeAlpha(this.backgroundColor, this.alpha, x);
+            fadeAlpha(this.textColor, this.alpha, x);
+            fadeAlpha(this.shadowColor, this.shadowAlpha, x);
 
             if(this.timer.check())
             {
@@ -113,18 +108,16 @@ public class GuiPopUpModal extends GuiModal
         else if (this.timer != null && !this.timer.check()) //if the mouse came back before the timer ended
         {
             this.timer = null;
-            this.bg_c = BACKGROUND_COLOR;
-            this.tx_c = TEXT_COLOR;
-            this.ds_c = McLib.primaryColor.get();
+            this.backgroundColor = BACKGROUND_COLOR.copy();
+            this.textColor = TEXT_COLOR.copy();
+            this.shadowColor = new Color(McLib.primaryColor.get()).set(this.shadowAlpha, 4);
         }
 
         GlStateManager.alphaFunc(GL11.GL_GREATER, 0.1F);
     }
 
-    private int fadeAlpha(int color, float x)
+    private void fadeAlpha(Color color, float alpha, float x)
     {
-        ColorUtils.COLOR.set(color, true).a = Interpolations.lerp(this.a, 0F, x);
-
-        return ColorUtils.COLOR.getRGBAColor();
+        color.a = Interpolations.lerp(alpha, 0F, x);
     }
 }
