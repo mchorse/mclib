193653976b001e4538cab02be9420f82e3c09e58
Chryfi <71967555+Chryfi@users.noreply.github.com>
2022-07-01 18:39:17 +0200

    Add class docs to ValueSerializer

--- src/main/java/mchorse/mclib/utils/ValueSerializer.java
+++ src/main/java/mchorse/mclib/utils/ValueSerializer.java
@@ -13,42 +13,95 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+/**
+ * A Serializer for GenericValue instances.
+ * <br><br>
+ * <h2>Ideal usage:</h2>
+ * All the variables that should be serialized/deserialized need to be GenericValue subclasses.
+ * Ideally, the references to these instances will not change throughout their lifetime, so the usage of final is advised.
+ * In the constructor of the class with the variables, the references to all the GenericValue variables 
+ * can now be registered to a ValueSerializer instance, 
+ * using {@link #registerValue(String, String, GenericValue)} or {@link #registerJSONValue(String, GenericValue)} or {@link #registerNBTValue(String, GenericValue)}.
+ */
 public class ValueSerializer implements IByteBufSerializable, INBTSerializable
 {
+    /**
+     * The key is a UUID String.
+     * This HashMap will not include duplicate GenericValue references.
+     */
     private final HashMap<String, GenericValue<?>> pool = new HashMap<>();
+
+    /**
+     * Key is the name that is used for serialization/deserialization
+     * Value is the UUID String pointing to the key of {@link #pool}
+     */
     private final HashMap<String, String> nbtMap = new HashMap<>();
     private final HashMap<String, String> jsonMap = new HashMap<>();
 
+    /**
+     * Register the provided GenericValue object reference
+     * with the provided key name for NBT serialization/deserialization.
+     * If the provided value is null or if the key already exists, it will not be registered.
+     *
+     * @param nbt name that should be used for serialization/deserialization
+     *            If it is empty, the value will not be registered for NBT serialization/deserialization
+     * @param value the reference to the value that should be serialization/deserialization
+     */
     public void registerNBTValue(String nbt, GenericValue<?> value)
     {
         this.registerValue(nbt, "", value);
     }
 
-    public void registerJSONValue(String jsonMap, GenericValue<?> value)
+    /**
+     * Register the provided GenericValue object reference
+     * with the provided key name for JSON serialization/deserialization.
+     * If the provided value is null or if the key already exists, it will not be registered.
+     *
+     * @param json name that should be used for serialization/deserialization
+     *                If it is empty, the value will not be registered for JSON serialization/deserialization
+     * @param value the reference to the value that should be serialization/deserialization
+     */
+    public void registerJSONValue(String json, GenericValue<?> value)
     {
-        this.registerValue("", jsonMap, value);
+        this.registerValue("", json, value);
     }
 
-    public void registerValue(String nbt, String jsonMap, GenericValue<?> value)
+    /**
+     * Register the provided GenericValue object reference
+     * with the provided key names for NBT and JSON serialization/deserialization.
+     * If the provided value is null or if the key already exists, it will not be registered.
+     *
+     * @param nbt name that should be used for serialization and deserialization.
+     *            If it is empty, the value will not be registered for NBT serialization/deserialization
+     * @param json name that should be used for serialization/deserialization
+     *                If it is empty, the value will not be registered for JSON serialization/deserialization
+     * @param value the reference to the value that should be serialization/deserialization
+     */
+    public void registerValue(String nbt, String json, GenericValue<?> value)
     {
+        if (value == null) return;
+
         if (!nbt.isEmpty() && !this.nbtMap.containsKey(nbt))
         {
             this.nbtMap.put(nbt, this.poolValue(value));
         }
 
-        if (!jsonMap.isEmpty() && !this.jsonMap.containsKey(jsonMap))
+        if (!json.isEmpty() && !this.jsonMap.containsKey(json))
         {
-            this.jsonMap.put(jsonMap, this.poolValue(value));
+            this.jsonMap.put(json, this.poolValue(value));
         }
     }
 
     /**
-     * Only pool the value if the object is not pooled yet.
+     * Only pool the value if the object is not pooled yet. Null is not going to be added to the pool.
      * @param value
      * @return the new uuid string of the pooled value, or if the value already existed return the uuid of it.
+     *         If the provided value was null, null will be returned.
      */
     protected String poolValue(GenericValue<?> value)
     {
+        if (value == null) return null;
+
         String uuid = this.getValueReference(value);
 
         if (uuid == null)
