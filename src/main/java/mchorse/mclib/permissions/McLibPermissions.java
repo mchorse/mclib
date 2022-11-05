99e18d87ab0d0b16f1d23942c9bc651768ef4f4a
Chryfi <71967555+Chryfi@users.noreply.github.com>
2022-11-05 19:48:57 +0100

    Register permissions via events now

---
+++ src/main/java/mchorse/mclib/permissions/McLibPermissions.java
@@ -0,0 +1,7 @@
+package mchorse.mclib.permissions;
+
+public class McLibPermissions
+{
+    public static PermissionCategory configEdit;
+    public static PermissionCategory accessGui;
+}
--- src/main/java/mchorse/mclib/permissions/PermissionUtils.java
+++ src/main/java/mchorse/mclib/permissions/PermissionUtils.java
@@ -2,14 +2,9 @@ package mchorse.mclib.permissions;
 
 import mchorse.mclib.network.mclib.Dispatcher;
 import mchorse.mclib.network.mclib.client.ClientHandlerAnswer;
-import mchorse.mclib.network.mclib.common.PacketRequestPermissions;
+import mchorse.mclib.network.mclib.common.PacketRequestPermission;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraftforge.server.permission.PermissionAPI;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 import java.util.function.Consumer;
 
 public class PermissionUtils
@@ -21,54 +16,15 @@ public class PermissionUtils
      * @param callback the callback to be executed after checking the player
      *                 or after the server has sent the permission result.
      */
-    public static void hasPermission(EntityPlayer player, String node, Consumer<Boolean> callback)
+    public static void hasPermission(EntityPlayer player, PermissionCategory permission, Consumer<Boolean> callback)
     {
         if (!Minecraft.getMinecraft().world.isRemote)
         {
-            callback.accept(PermissionAPI.hasPermission(player, node));
+            callback.accept(permission.playerHasPermission(player));
         }
         else
         {
-            ClientHandlerAnswer.requestServerAnswer(Dispatcher.DISPATCHER, new PacketRequestPermissions(-1, node), (obj) ->
-            {
-                if (obj instanceof Map)
-                {
-                    Object[] values = ((Map) obj).values().toArray();
-
-                    if (values.length > 0 && values[0] instanceof Boolean)
-                    {
-                        callback.accept((Boolean) values[0]);
-
-                        System.out.println("" + values[0]);
-                    }
-                }
-            });
-        }
-    }
-
-    /**
-     * Side independent method for checking if the player has a set of permissions and then passing the result to the callback
-     * @param nodes the full name of the permission
-     * @param player the player to check for the permission
-     * @param callback the callback to be executed after checking the player
-     *                 or after the server has sent the permission result.
-     */
-    public static void hasPermissions(EntityPlayer player, Set<String> nodes, Consumer<HashMap<String, Boolean>> callback)
-    {
-        if (!Minecraft.getMinecraft().world.isRemote)
-        {
-            HashMap<String, Boolean> results = new HashMap<>();
-
-            for (String node : nodes)
-            {
-                results.put(node, PermissionAPI.hasPermission(player, node));
-            }
-
-            callback.accept(results);
-        }
-        else
-        {
-            ClientHandlerAnswer.requestServerAnswer(Dispatcher.DISPATCHER, new PacketRequestPermissions(-1, nodes), callback);
+            ClientHandlerAnswer.requestServerAnswer(Dispatcher.DISPATCHER, new PacketRequestPermission(-1, permission), callback);
         }
     }
 }
