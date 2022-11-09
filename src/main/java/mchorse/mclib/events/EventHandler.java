7408aaccd2a0d161a0aadab384fbfd2402a5ff46
Chryfi <71967555+Chryfi@users.noreply.github.com>
2022-11-09 18:10:43 +0100

    Remove client server time sync
    Java's System.currentTimeMillis() already returns the time in UTC - there is no need to sync for different timezones or other differences.

--- src/main/java/mchorse/mclib/events/EventHandler.java
+++ src/main/java/mchorse/mclib/events/EventHandler.java
@@ -1,6 +1,5 @@
 package mchorse.mclib.events;
 
-import mchorse.mclib.network.mclib.client.ClientHandlerTimeSync;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.PlayerTickEvent;
@@ -10,17 +9,8 @@ import net.minecraftforge.fml.relauncher.Side;
 public class EventHandler
 {
     @SubscribeEvent
-    public void playerTickEvent(PlayerTickEvent event)
-    {
-        if (event.side == Side.CLIENT && event.phase == TickEvent.Phase.START && !ClientHandlerTimeSync.isSet() && ClientHandlerTimeSync.canPing())
-        {
-            ClientHandlerTimeSync.requestServerTime();
-        }
-    }
-
-    @SubscribeEvent
     public void clientLogsOut(ClientDisconnectionFromServerEvent event)
     {
-        ClientHandlerTimeSync.resetTimes();
+
     }
 }
--- src/main/java/mchorse/mclib/network/mclib/Dispatcher.java
+++ src/main/java/mchorse/mclib/network/mclib/Dispatcher.java
@@ -6,7 +6,6 @@ import mchorse.mclib.network.mclib.client.ClientHandlerAnswer;
 import mchorse.mclib.network.mclib.client.ClientHandlerBoolean;
 import mchorse.mclib.network.mclib.client.ClientHandlerConfig;
 import mchorse.mclib.network.mclib.client.ClientHandlerConfirm;
-import mchorse.mclib.network.mclib.client.ClientHandlerTimeSync;
 import mchorse.mclib.network.mclib.common.PacketAnswer;
 import mchorse.mclib.network.mclib.common.PacketBoolean;
 import mchorse.mclib.network.mclib.common.PacketConfig;
@@ -14,13 +13,11 @@ import mchorse.mclib.network.mclib.common.PacketConfirm;
 import mchorse.mclib.network.mclib.common.PacketDropItem;
 import mchorse.mclib.network.mclib.common.PacketRequestConfigs;
 import mchorse.mclib.network.mclib.common.PacketRequestPermission;
-import mchorse.mclib.network.mclib.common.PacketTime;
 import mchorse.mclib.network.mclib.server.ServerHandlerConfig;
 import mchorse.mclib.network.mclib.server.ServerHandlerConfirm;
 import mchorse.mclib.network.mclib.server.ServerHandlerDropItem;
 import mchorse.mclib.network.mclib.server.ServerHandlerPermissionRequest;
 import mchorse.mclib.network.mclib.server.ServerHandlerRequestConfigs;
-import mchorse.mclib.network.mclib.server.ServerHandlerTimeSync;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
@@ -48,10 +45,6 @@ public class Dispatcher
             /* client answer related packets */
             register(PacketAnswer.class, ClientHandlerAnswer.class, Side.CLIENT);
             register(PacketBoolean.class, ClientHandlerBoolean.class, Side.CLIENT);
-
-            /* to let the client know what time the server has */
-            register(PacketTime.class, ClientHandlerTimeSync.class, Side.CLIENT);
-            register(PacketTime.class, ServerHandlerTimeSync.class, Side.SERVER);
 
             register(PacketRequestPermission.class, ServerHandlerPermissionRequest.class, Side.SERVER);
         }
