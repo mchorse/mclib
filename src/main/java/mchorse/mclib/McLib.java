074888e4f1067a1d24438d651ac44ab2a4814f56
Sekwah <contact@sekwah.com>
2023-05-08 12:46:44 +0100

    chore: update to the latest forgegradle

--- src/main/java/mchorse/mclib/McLib.java
+++ src/main/java/mchorse/mclib/McLib.java
@@ -42,11 +42,10 @@ import java.util.Map;
  * All it does is provides common code for McHorse's mods.
  */
 @Mod.EventBusSubscriber
-@Mod(modid = McLib.MOD_ID, name = "McLib", version = McLib.VERSION, updateJSON = "https://raw.githubusercontent.com/mchorse/mclib/1.12/version.json")
+@Mod(modid = McLib.MOD_ID, name = "McLib", updateJSON = "https://raw.githubusercontent.com/mchorse/mclib/1.12/version.json")
 public class McLib
 {
     public static final String MOD_ID = "mclib";
-    public static final String VERSION = "%VERSION%";
 
     /* Proxies */
     public static final String CLIENT_PROXY = "mchorse.mclib.ClientProxy";
